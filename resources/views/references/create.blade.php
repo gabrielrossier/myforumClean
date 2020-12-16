989d5c5dfae58c2d9683ef2553c25acb574ed003
MisterX25 <Xavier.Carrel@cpnv.ch>
2020-12-16 10:05:20 +0100

    CSRF token in form body (shared between views)

--- resources/views/references/create.blade.php
+++ resources/views/references/create.blade.php
@@ -2,7 +2,6 @@
 
 @section ('content')
     <form action="{{ route('references.store') }}" method="post">
-        @csrf
         @include('references.form_body')
         <div class="row m-3">
             <button class="btn btn-success">Enregistrer</button>
--- resources/views/references/update.blade.php
+++ resources/views/references/update.blade.php
@@ -3,10 +3,9 @@
 @section ('content')
     <form action="{{ route('references.update',$reference->id) }}" method="post">
         @method('PUT')
-        @csrf
         @include('references.form_body')
         <div class="row m-3">
-            <button class="btn btn-success">Enregistrer</button>
+            <button class="btn btn-success">Mettre à jour</button>
             <a href="{{ url()->previous() }}" class="btn btn-primary">Annuler</a>
         </div>
     </form>
