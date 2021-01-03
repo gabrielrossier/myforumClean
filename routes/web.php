c970f787b282458a6748c8a757606cdb8ea1ec8a
MisterX25 <Xavier.Carrel@cpnv.ch>
2021-01-03 17:35:52 +0100

    scaffolded breeze

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\OpinionController;
-use App\Http\Controllers\ThemeController;
-use App\Http\Controllers\ReferenceController;
-use App\Http\Controllers\RoleController;
-use App\Http\Controllers\StateController;
-use App\Http\Controllers\TopicController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,12 +13,12 @@ use App\Http\Controllers\TopicController;
 |
 */
 
-Route::get('/', 'App\Http\Controllers\HomeController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::Resource('opinions',OpinionController::class);
-Route::Resource('references',ReferenceController::class);
-Route::Resource('roles',RoleController::class);
-Route::Resource('states',StateController::class);
-Route::Resource('themes',ThemeController::class);
-Route::Resource('topics',TopicController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
+require __DIR__.'/auth.php';
