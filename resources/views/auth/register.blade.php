e8cc5ba9a6f6f66c4be910afa3fd1939c479280d
MisterX25 <Xavier.Carrel@cpnv.ch>
2021-01-03 18:12:53 +0100

    Lien pour s'enregistrer

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,6 @@
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
             </a>
         </x-slot>
 
@@ -14,21 +13,21 @@
 
             <!-- Name -->
             <div>
-                <x-label for="name" :value="__('Name')" />
+                <label for="name">Nom d'utilisateur</label>
 
                 <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
             </div>
 
             <!-- Email Address -->
             <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
+                <label for="email">Email</label>
 
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
             </div>
 
             <!-- Password -->
             <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+                <label for="password">Mot de passe</label>
 
                 <x-input id="password" class="block mt-1 w-full"
                                 type="password"
@@ -38,7 +37,7 @@
 
             <!-- Confirm Password -->
             <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
+                <label for="password_confirmation">Confirmation</label>
 
                 <x-input id="password_confirmation" class="block mt-1 w-full"
                                 type="password"
@@ -47,11 +46,11 @@
 
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
+                    Déjà enregistré ?
                 </a>
 
                 <x-button class="ml-4">
-                    {{ __('Register') }}
+                    S'enregistrer
                 </x-button>
             </div>
         </form>
