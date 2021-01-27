e62785357acceddc336ac7457817c797df2f5fc5
MisterX25 <Xavier.Carrel@cpnv.ch>
2021-01-27 15:05:15 +0100

    Admin privilege evaluated in a policy

---
+++ app/Policies/AdminPolicy.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Support\Facades\Auth;
+
+class AdminPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Tells if the current user has management privileges in general
+     * @return bool
+     */
+    public function manage()
+    {
+        return Auth::user() && Auth::user()->role->slug == 'ADMI';
+    }
+}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Opinion;
+use App\Policies\AdminPolicy;
 use App\Policies\OpinionPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Auth;
@@ -27,9 +28,6 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        Gate::define('manage', function () {
-            return Auth::user() && Auth::user()->role->slug == 'ADMI';
-        });
+        Gate::define('manage', [AdminPolicy::class, 'manage']);
     }
 }
